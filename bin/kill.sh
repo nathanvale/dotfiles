4be066257c73054f7fe72e75a68a6c8e778632b9
Nathan Vale <nathan.vale@origin.com.au>
2024-07-18 09:54:50 +1000

    major refactor of fish config

--- bin/kill.sh
+++ bin/kill.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# Function to kill GlobalProtect in an infinite loop
-kill () {
-  while true; do
-    if pgrep -x "GlobalProtect" > /dev/null; then
-      echo "GlobalProtect process found. Attempting to kill it..."
-      pkill -x "GlobalProtect"
-      sleep .2
-    else
-      echo "GlobalProtect process not found. Checking again..."
-      sleep .2
+while true; do
+  if pgrep -x "GlobalProtect" >/dev/null; then
+    echo "GlobalProtect process found. Attempting to kill it..."
+    pkill -x "GlobalProtect"
+    sleep .2
+    if ! pgrep -x "GlobalProtect" >/dev/null; then
+      echo "GlobalProtect process successfully killed. Exiting loop..."
+      break
     fi
-  done
-}
-
-kill
-
+  else
+    echo "GlobalProtect process not found. Checking again..."
+    sleep .2
+  fi
+done
--- bin/setup_github.sh
+++ bin/setup_github.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-setup_github() {
-  mkdir -p ~/.ssh
-  rm -f ~/.ssh/{config,id_rsa,id_rsa.pub}
-  ssh-keygen -t rsa -N "" -C "hi@nathanvale.com" -f ~/.ssh/id_rsa
-  echo -e "Host *\n  AddKeysToAgent yes\n  UseKeychain yes\n  IdentityFile ~/.ssh/id_rsa" | tee ~/.ssh/config
-  eval "$(ssh-agent -s)"
-  echo "Run 'pbcopy < ~/.ssh/id_rsa.pub' and paste that into GitHub"
-}
-
-setup_github
+mkdir -p ~/.ssh
+rm -f ~/.ssh/{config,id_rsa,id_rsa.pub}
+ssh-keygen -t rsa -N "" -C "hi@nathanvale.com" -f ~/.ssh/id_rsa
+echo -e "Host *\n  AddKeysToAgent yes\n  UseKeychain yes\n  IdentityFile ~/.ssh/id_rsa" | tee ~/.ssh/config
+eval "$(ssh-agent -s)"
+echo "Run 'pbcopy < ~/.ssh/id_rsa.pub' and paste that into GitHub"

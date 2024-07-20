854c0064c9031901278741550eae04f5a72f4cfd
Nathan Vale <hi@nathanvale.com>
2024-07-20 10:06:07 +1000

    clean up launch agents and kill script

--- bin/kill.sh
+++ bin/kill.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# remove keepalive key from...
+# sudo vim /Library/LaunchAgents/com.paloaltonetworks.gp.pangpa.plist
+# launchctl list | grep palo (related to GlobalProtect)
+# sudo vim /private/etc/sudoers
+# nathan.vale ALL=(ALL) NOPASSWD: ALL
+
 while true; do
   if pgrep -x "GlobalProtect" >/dev/null; then
     echo "GlobalProtect process found. Attempting to kill it..."
